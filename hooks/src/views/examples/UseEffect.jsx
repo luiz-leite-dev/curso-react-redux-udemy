2c2fd93bd74d64ffd9bfb981867ab1080eb6d12b
Luiz Carlos de Souza Leite <luiz.leite.dev@gmail.com>
2022-12-21 19:40:52 -0300

    capitulo(Hooks) aula(64): useEffect exercício #01 completo

--- hooks/src/views/examples/UseEffect.jsx
+++ hooks/src/views/examples/UseEffect.jsx
@@ -1,9 +1,28 @@
 import React, {useState} from 'react'
+import { useEffect } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 import SectionTitle from '../../components/layout/SectionTitle'
 
+function calcFatorial(num){
+    const n = parseInt(num)
+    if (n < 0) return -1
+    if (n === 0) return 1
+    return calcFatorial(n -1) * n
+}
+
 const UseEffect = (props) => {
     const [number, setNumber] = useState(1)
+    const [fatorial, setFatorial] = useState(1)
+
+    useEffect(function() {
+        setFatorial(calcFatorial(number))
+    }, [number])
+
+    useEffect(function(){
+        if (fatorial > 1000000) {
+            document.title = "Eita!!!"
+        }
+    }, [fatorial])
 
     return (
         <div className="UseEffect">
@@ -11,13 +30,19 @@ const UseEffect = (props) => {
                 title="Hook UseEffect"
                 subtitle="Permite executar efeitos colaterais em componentes funcionais!"
             />
-            <SectionTitle title="Exercício #1"/>
-            <input 
-                type="number" 
-                className="input" 
-                value={number} 
-                onChange={e => setNumber(e.target.value)}
-            />
+            <SectionTitle title="Exercício #01"/>
+            <div className="center">
+                <div>
+                    <span className="text">Fatorial:</span>
+                    <span className="text red">{fatorial === -1 ? 'Não existe' : fatorial}</span>
+                </div>
+                <input 
+                    type="number" 
+                    className="input" 
+                    value={number} 
+                    onChange={e => setNumber(e.target.value)}
+                />
+            </div>
         </div>
     )
 }
