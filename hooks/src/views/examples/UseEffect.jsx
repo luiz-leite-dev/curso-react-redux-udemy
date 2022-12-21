9e816f1960cadbde605f788db241f7eca4366a2f
Luiz Carlos de Souza Leite <luiz.leite.dev@gmail.com>
2022-12-21 19:20:44 -0300

    capitulo(Hooks) aula(64): useEffect exercício #1

--- hooks/src/views/examples/UseEffect.jsx
+++ hooks/src/views/examples/UseEffect.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
 const UseEffect = (props) => {
+    const [number, setNumber] = useState(1)
+
     return (
         <div className="UseEffect">
             <PageTitle
                 title="Hook UseEffect"
                 subtitle="Permite executar efeitos colaterais em componentes funcionais!"
+            />
+            <SectionTitle title="Exercício #1"/>
+            <input 
+                type="number" 
+                className="input" 
+                value={number} 
+                onChange={e => setNumber(e.target.value)}
             />
         </div>
     )
