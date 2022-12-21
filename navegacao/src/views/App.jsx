cf4a3df7b0132ffa30eb9c30e5993b0094907d2b
Luiz Carlos de Souza Leite <luiz.leite.dev@gmail.com>
2022-12-21 14:12:32 -0300

    feat(Navecacao): aula 53 - aplicando estilo

--- navegacao/src/views/App.jsx
+++ navegacao/src/views/App.jsx
@@ -1,8 +1,12 @@
 import './App.css'
 
+import Menu from '../components/layout/Menu'
+import Content from '../components/layout/Content'
+
 const App = props => (
     <div className="App">
-        App
+        <Menu />
+        <Content />
     </div>
 )
 
